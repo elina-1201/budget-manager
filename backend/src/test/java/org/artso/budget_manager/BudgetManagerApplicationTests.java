570b0361170a82fc60cf678689a32cc64277dcb7
elina-1201 <elina.mursalimova16@gmail.com>
2026-04-22 15:53:12 +0200

    Initial commit

---
+++ backend/src/test/java/org/artso/budget_manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.artso.budget_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
